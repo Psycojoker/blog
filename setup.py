571eb4cabfd7455043e5c10ea1cb3760e82d8ff3
Laurent Peuch <cortex@worlddomination.be>
2015-07-06 02:02:12 +0200

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='blog',
+      version='0.1',
+      description='utilitary for my own blogging',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/blog',
+      install_requires=['pillow', 'argh', 'unidecode'],
+      packages=[],
+      py_modules=[],
+      license= 'wtfpl',
+      scripts=['blog'],
+      keywords='',
+     )
